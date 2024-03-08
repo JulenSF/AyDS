fa1a51106c4cf379afba2c6794b83316d82ad07c
Julen <113545463+JulenSF@users.noreply.github.com>
2024-03-08 13:13:00 +0100

    Add files via upload

---
+++ InfoNutricionalUnidad.java
@@ -0,0 +1,6 @@
+public class InfoNutricionalUnidad extends InfoNutricional {
+    public InfoNutricionalUnidad(double calorías, double hidratosCarbono, double grasasTotales, double grasasSaturadas, double proteínas, double azúcares, double fibra, double sodio) {
+        super(calorías, hidratosCarbono, grasasTotales, grasasSaturadas, proteínas, azúcares, fibra, sodio);
+        this.name = "UNIDAD";
+    }
+}
